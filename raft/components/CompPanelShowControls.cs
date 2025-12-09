5ca8d84e991951990a561df1d5f67289bd7c4afc
BarrelDevelopment <aayi.ijay@gmail.com>
2025-12-09 16:47:16 +0100

    Implement sample

--- raft/components/CompPanelShowControls.cs
+++ raft/components/CompPanelShowControls.cs
@@ -5,10 +5,10 @@ namespace raft.components;
 //TODO: Will not be change anymore. Change to static?
 //But how will it work with the ui manager? Are we fucked up?
 public class CompPanelShowControls {
-    public Panel Panel;
+    public Panel? Panel { get; set; }
 
     // ReSharper disable once ConvertConstructorToMemberInitializers
     public CompPanelShowControls() {
-        Panel = new Panel("Some UI here");
+        Panel = new Panel(new Text("Show controls here. The size of this panel will be changed as soon as I found out how this shit works").Centered()).Expand();
     }
 }

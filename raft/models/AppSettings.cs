239ca2ed02cae266a0b78bd51329d6f7378628af
BarrelDevelopment <aayi.ijay@gmail.com>
2025-12-11 14:14:09 +0100

    Update AppSettings.cs

--- raft/models/AppSettings.cs
+++ raft/models/AppSettings.cs
@@ -5,4 +5,7 @@ using Spectre.Console;
 public class AppSettings {
     public Profil? LastLoadedProfil { get; set; }
     public Style? MainBorderStyle { get; set; }
+    public int ConsolenWidth { get; set; }
+    public int ConsolenHeight { get; set; }
+    public int MainLayoutPadding { get; set; }
 }

204f088c802d5796586db555301cf3ae71760f33
BarrelDevelopment <aayi.ijay@gmail.com>
2025-12-16 13:41:56 +0100

    Create calendar model and manager

---
+++ raft/managers/CalendarManager.cs
@@ -0,0 +1,7 @@
+using raft.models;
+
+namespace raft.Managers;
+
+public class CalendarManager {
+    private List<RaftCalendarModel> calendars = new List<RaftCalendarModel>();
+}
---
+++ raft/models/RaftCalendarModel.cs
@@ -0,0 +1,12 @@
+using raft.widgets;
+using Spectre.Console;
+
+namespace raft.models;
+
+public class RaftCalendarModel {
+    public RaftCalendar Calendar { get; set; }
+    
+    
+    
+    
+}

8783311a199545b4ffde508f9f9e4c094e4b8496
BarrelDevelopment <aayi.ijay@gmail.com>
2025-12-18 08:23:44 +0100

    Update CalendarManager.cs

--- raft/managers/CalendarManager.cs
+++ raft/managers/CalendarManager.cs
@@ -1,7 +1,14 @@
 using raft.models;
+using raft.widgets;
 
 namespace raft.Managers;
 
 public class CalendarManager {
     private List<RaftCalendarModel> calendars = new List<RaftCalendarModel>();
+
+    public int[] GetGridFromCalendar(RaftCalendar calendar) {
+        int[] grid = new int[2];
+        return grid;
+    }
+    
 }

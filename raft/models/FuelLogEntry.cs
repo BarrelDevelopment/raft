87ee840907d0a08228c349fb56440654d126d449
BarrelDevelopment <aayi.ijay@gmail.com>
2026-01-12 12:53:07 +0100

    Combine date and time property

--- raft/models/FuelLogEntry.cs
+++ raft/models/FuelLogEntry.cs
@@ -1,8 +1,7 @@
 namespace raft.models;
 
 public class FuelLogEntry {
-    public DateOnly Date { get; set; }
-    public DateTime Time { get; set; }
+    public DateTime DateTime { get; set; }
     public float Amount { get; set; }
     public float Volume { get; set; }
 }

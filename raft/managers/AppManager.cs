c8a2d27a9ed821a1cbfd66ba9aea80f8fb9ce0d3
BarrelDevelopment <aayi.ijay@gmail.com>
2026-01-16 09:37:00 +0100

    Update AppManager.cs

--- raft/managers/AppManager.cs
+++ raft/managers/AppManager.cs
@@ -9,12 +9,11 @@ using Spectre.Console;
 namespace raft.Managers;
 
 public class AppManager {
-    private readonly UserInputManager userInputManager;
-    private AppSettings Settings { get; }
-    private UiManager UiManager { get; }
-
-    private SessionManager SessionManager { get; }
-
+    private readonly UserInputManager _userInputManager;
+    private readonly UiManager _uiManager;
+    private readonly SessionManager _sessionManager;
+    
+    private readonly AppSettings _settings;
     private bool _isAppRunning;
 
     public CalendarAnnual CalendarModel { get; private set; } = new() {
@@ -24,24 +23,24 @@ public class AppManager {
     };
 
     public AppManager() {
-        Settings = AppSettings.LoadAppSettings();
-        UiManager = new UiManager(Settings, this);
-        SessionManager = new SessionManager(Settings);
-        userInputManager = new UserInputManager();
-        userInputManager.CommandIssued += OnCommandIssued;
+        _settings = AppSettings.LoadAppSettings();
+        _uiManager = new UiManager(_settings, this);
+        _sessionManager = new SessionManager(_settings);
+        _userInputManager = new UserInputManager();
+        _userInputManager.CommandIssued += OnCommandIssued;
     }
 
 
     public void Run() {
         _isAppRunning = true;
 
-        AnsiConsole.Live(UiManager.mainLayoutView.Layout)
+        AnsiConsole.Live(_uiManager.mainLayoutView.Layout!)
             .Start(ctx => {
                 while (_isAppRunning) {
-                    UiManager.UpdateUi(ctx);
-                    userInputManager.GetUserInput();
+                    _uiManager.UpdateUi(ctx);
+                    _userInputManager.GetUserInput();
                 }
-                AppSettings.SaveAppSettings(Settings);
+                AppSettings.SaveAppSettings(_settings);
             });
     }
 
@@ -66,7 +65,10 @@ public class AppManager {
     }
     private void NextCalendarDay() { }
     private void NextCalendarMonth() { }
-    private void Save() { }
+
+    private void Save() {
+        
+    }
 
     private void Exit() {
         _isAppRunning = false;
